f7d02e14d24275d09dfbb4187132458e1ae5c6c3
Himanshi0901 <himanshi9104@gmail.com>
2025-01-01 19:38:58 +0530

    Return the code for the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./Weather_App/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        aqi: {
+          1: 'var(--aqi-1)',
+          2: 'var(--aqi-2)',
+          3: 'var(--aqi-3)',
+          4: 'var(--aqi-4)',
+          5: 'var(--aqi-5)',
+        },
+      },
+    },
   },
   plugins: [],
 }
